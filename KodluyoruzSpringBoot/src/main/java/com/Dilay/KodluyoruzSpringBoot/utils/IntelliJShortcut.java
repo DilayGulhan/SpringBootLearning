efb6841635ea2542c68c4729542960e42fa8f3ca
DilayGulhan <dilaygulhan@gmail.com>
2023-03-21 16:31:35 +0300

    Shortcuts for IntelliJ

---
+++ KodluyoruzSpringBoot/src/main/java/com/Dilay/KodluyoruzSpringBoot/utils/IntelliJShortcut.java
@@ -0,0 +1,9 @@
+package com.Dilay.KodluyoruzSpringBoot.utils;
+
+public class IntelliJShortcut {
+    // Setting ==> ctrl+alt+s
+    //Generate Menu ==> Alt+ Insert
+   //Ctrl+/ ==> single comment
+// Ctrl+shift+/ ==> multiple comment
+
+}
---
+++ KodluyoruzSpringBoot/src/main/java/com/Dilay/KodluyoruzSpringBoot/utils/PermaLink.java
@@ -0,0 +1,6 @@
+package com.Dilay.KodluyoruzSpringBoot.utils;
+
+public class PermaLink {
+
+    //  // GITHUB : https://github.com/DilayGulhan/SpringBootLearning
+}
