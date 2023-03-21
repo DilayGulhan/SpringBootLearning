1835569e408ed7c4f0ebd8ee5aa401cb7c8f67b2
DilayGulhan <dilaygulhan@gmail.com>
2023-03-21 16:32:48 +0300

    Main

---
+++ KodluyoruzSpringBoot/src/main/java/com/Dilay/KodluyoruzSpringBoot/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.Dilay.KodluyoruzSpringBoot.utils;
+
+public class ConfigProgramming {
+//    DevTools ==> LiveReload
+//   file ==> settings ==> compiler ==> BuildProject automatically(ok)
+// file ==> Advanced Settings ==> Allow-make to start even(ok)
+}
